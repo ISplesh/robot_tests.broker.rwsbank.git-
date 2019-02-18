95c5190958e68edae5fd2d8e1b6ab94a8e0acc67
Kirill_Bernatskiy <cloud_mail@i.ua>
2019-02-18 15:45:54 +0200

    add or change tests and other information for rwsbank in rwsbank.robot and rwsbank_service.py

--- rwsbank_service.py
+++ rwsbank_service.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from iso8601 import parse_date
 import pytz
 import urllib
+import re
 import os
 
 
 tz = str(datetime.now(pytz.timezone('Europe/Kiev')))[26:]
 
 
+def prepare_tender_data_asset(tender_data):
+    tender_data['data']['assetCustodian']['identifier']['id'] = u'12655674'
+    tender_data['data']['assetCustodian']['name'] = u'Jmail'
+    tender_data['data']['assetCustodian']['identifier']['legalName'] = u'Jmail'
+    tender_data['data']['assetCustodian']['contactPoint']['name'] = u'dasdasdas'
+    tender_data['data']['assetCustodian']['contactPoint']['telephone'] = u'+38(123)123-32-31'
+    tender_data['data']['assetCustodian']['contactPoint']['email'] = u'cloudemai.l.40@gmail.com'
+    for item in range(len(tender_data['data']['items'])):
+        if tender_data['data']['items'][item]['address']['region'] == u'місто Київ':
+            tender_data['data']['items'][item]['address']['region'] = u'Київ'
+    return tender_data
+
+
 def prepare_tender_data(role, data):
-    if role == 'tender_owner':
-        data['data']['procuringEntity']['name'] = u'Ubisoft'
+    if role == 'tender_owner' and 'assetCustodian' in data['data']:
+        data = prepare_tender_data_asset(data)
     return data
 
 
@@ -20,14 +35,11 @@ def convert_date_from_item(date):
     return '{}T00:00:00{}'.format(date, tz)
 
 
-def adapt_paid_date(sign_date, date_paid):
-    time = sign_date[-8:]
-    date = datetime.strptime(date_paid, '%Y-%m-%d')
-    return '{} {}'.format(datetime.strftime(date, '%d/%m/%Y'), time)
-
-
 def convert_date(date):
-    date = datetime.strptime(date, '%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
+    if '.' in date:
+        date = datetime.strptime(date, '%d.%m.%Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
+    else:
+        date = datetime.strptime(date, '%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f')
     return '{}{}'.format(date, tz)
 
 
@@ -36,21 +48,39 @@ def convert_date_for_item(date):
     return '{}'.format(date)
 
 
+def convert_date_for_date_paid(date):
+    iso_dt = parse_date(date)
+    date_string = iso_dt.strftime("%d.%m.%Y %H:%M")
+    return date_string
+
+
 def convert_date_for_auction(date):
-    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f{}'.format(tz)).strftime('%d/%m/%Y %H:%M')
+    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f{}'.format(tz)).strftime('%d/%m/%Y %H:%M:%S')
     return '{}'.format(date)
 
-def convert_date_for_datePaid(date):
-    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f{}'.format(tz)).strftime('%d/%m/%Y %H:%M:%S')
-    return date
+
+def convert_date_for_milestone():
+    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S'.format(tz)).strftime('%d.%m.%Y %H:%M')
+    return '{}'.format(date)
 
 
-def dgf_decision_date_from_site(date):
-    return u'{}-{}-{}'.format(date[-4:], date[-7:-5], date[-10:-8])
+def convert_date_from_decision(date):
+    date = datetime.strptime(date, '%d/%m/%Y'.format(tz)).strftime('%Y-%m-%dT%H:%M:%S.%f')
+    return '{}{}'.format(date, tz)
 
 
-def dgf_decision_date_for_site(date):
-    return u'{}/{}/{}'.format(date[-2:], date[-5:-3], date[-10:-6])
+def convert_date_for_decision(date):
+    date = datetime.strptime(date, '%Y-%m-%d'.format(tz)).strftime('%d/%m/%Y')
+    return '{}'.format(date)
+
+
+def convert_duration(duration):
+    if duration == u'P1M':
+        duration = u'P30D'
+    days = re.search('\d+D|$', duration).group()
+    if len(days) > 0:
+        days = days[:-1]
+    return days
 
 
 def adapted_dictionary(value):
@@ -105,13 +135,13 @@ def adapted_dictionary(value):
 def adapt_data(field, value):
     if field == 'tenderAttempts':
         value = int(value)
-    elif 'dutchSteps' in field:
-        value = int(value)
     elif field == 'value.amount':
         value = float(value)
     elif field == 'minimalStep.amount':
         value = float(value.split(' ')[0])
     elif field == 'guarantee.amount':
+        value = float(value.split(' ')[0])
+    elif field == 'registrationFee.amount':
         value = float(value.split(' ')[0])
     elif field == 'quantity':
         value = float(value.replace(',', '.'))
@@ -119,14 +149,59 @@ def adapt_data(field, value):
         value = int(value)
     elif 'contractPeriod' in field:
         value = convert_date_from_item(value)
-    elif 'tenderPeriod' in field or 'auctionPeriod' in field or 'datePaid' in field:
+    elif 'tenderPeriod' in field or 'auctionPeriod' in field or 'rectificationPeriod' in field and 'invalidationDate' not in field:
         value = convert_date(value)
-    elif 'dgfDecisionDate' in field:
-        value = dgf_decision_date_from_site(value)
-    elif 'dgfDecisionID' in field:
-        value = value[-6:]
     else:
         value = adapted_dictionary(value)
+    return value
+
+
+def adapt_asset_data(field, value):
+    if 'date' in field:
+        value = convert_date(value)
+    elif 'decisionDate' in field:
+        value = convert_date_from_decision(value.split(' ')[0])
+    elif 'documentType' in field:
+        value = adapted_dictionary(value.split(' ')[0])
+    elif 'rectificationPeriod.endDate' in field:
+        value = convert_date(value)
+    elif 'documentType' in field:
+        value = value
+    else:
+        value = adapted_dictionary(value)
+    return value
+
+
+def adapt_lot_data(field, value):
+    if 'amount' in field:
+        value = float(value.split(' ')[0])
+    elif 'tenderingDuration' in field:
+        value = value.split(' ')[0]
+        if 'M' in value:
+            value = 'P{}'.format(value)
+        else:
+            value = 'P{}D'.format(value)
+    elif 'auctionPeriod.startDate' in field:
+        value = convert_date(value)
+    elif 'classification.id' in field:
+        value = value.split(' - ')[0]
+    elif 'unit.name' in field:
+        value = ' '.join(value.split(' ')[1:])
+    elif 'quantity' in field:
+        value = float(value.split(' ')[0])
+    elif 'registrationFee.amount' in field:
+        value = float(value.split(' ')[0])
+    elif 'tenderAttempts' in field:
+        value = int(value)
+    else:
+        value = adapted_dictionary(value)
+    return value
+
+
+def adapt_edrpou(value):
+    value = str(value)
+    if len(value) == 7:
+        value += '0'
     return value
 
 
@@ -134,7 +209,6 @@ def download_file(url, filename, folder):
     urllib.urlretrieve(url, ('{}/{}'.format(folder, filename)))
 
 
-def my_file_path():
-    return os.path.join(os.getcwd(), 'src', 'robot_tests.broker.rwsbank', 'Doc.pdf')
-
+def get_upload_file_path():
+    return os.path.join(os.getcwd(), 'src/robot_tests.broker.rwsbank/Doc.pdf')
 
